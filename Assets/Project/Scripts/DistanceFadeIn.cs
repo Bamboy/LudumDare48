9c183e8e730bd9263919151932cf5260c7fe6ea7
Conner Petzold <cpetzold@gmail.com>
2021-04-25 17:44:13 -0700

    Start screen work

---
+++ Assets/Project/Scripts/DistanceFadeIn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DistanceFadeIn : MonoBehaviour
+{
+    public TerrainController terrain;
+    TextMeshProUGUI text;
+
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        Color c = text.color;
+        c.a = VectorExtras.Remap(10, 80, 0, 1, terrain.GetPlayerDistance());
+        text.color = c;
+    }
+}
